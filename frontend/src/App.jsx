1d1a351e35bd4cc788fb51bc3820c0953bbde108
zainabfatima097 <zainabfatima.zaman@gmail.com>
2025-10-04 19:06:53 +0530

    updated app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,19 +5,23 @@ import Login from "./pages/login/Login";
 import SignUp from "./pages/signup/SignUp";
 import { Toaster } from "react-hot-toast";
 import { useAuthContext } from "./context/AuthContext";
+import MagicCursorTrail from "./components/MagicCursorTrail"; 
 
 function App() {
-	const { authUser } = useAuthContext();
-	return (
-		<div className='p-4 h-screen flex items-center justify-center'>
-			<Routes>
-				<Route path='/' element={authUser ? <Home /> : <Navigate to={"/login"} />} />
-				<Route path='/login' element={authUser ? <Navigate to='/' /> : <Login />} />
-				<Route path='/signup' element={authUser ? <Navigate to='/' /> : <SignUp />} />
-			</Routes>
-			<Toaster />
-		</div>
-	);
+  const { authUser } = useAuthContext();
+  
+  return (
+    <div className='p-4 h-screen flex items-center justify-center'>
+      <MagicCursorTrail />
+      
+      <Routes>
+        <Route path='/' element={authUser ? <Home /> : <Navigate to={"/login"} />} />
+        <Route path='/login' element={authUser ? <Navigate to='/' /> : <Login />} />
+        <Route path='/signup' element={authUser ? <Navigate to='/' /> : <SignUp />} />
+      </Routes>
+      <Toaster />
+    </div>
+  );
 }
 
 export default App;
