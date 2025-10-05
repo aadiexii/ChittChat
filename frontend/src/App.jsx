1fb65091aa79e9a6871eb96f7d1e1e566ba5c9f3
Shubham Kumar <skshubham8348@gmail.com>
2025-10-05 12:19:28 +0530

    create forgot & set password pages and integrate API

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,8 +3,10 @@ import "./App.css";
 import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
 import SignUp from "./pages/signup/SignUp";
+import ForgotPassword from "./pages/ForgotPassword/ForgotPassword";
 import { Toaster } from "react-hot-toast";
 import { useAuthContext } from "./context/AuthContext";
+
 
 function App() {
 	const { authUser } = useAuthContext();
@@ -13,6 +15,7 @@ function App() {
 			<Routes>
 				<Route path='/' element={authUser ? <Home /> : <Navigate to={"/login"} />} />
 				<Route path='/login' element={authUser ? <Navigate to='/' /> : <Login />} />
+				<Route path="/forgot-password" element={<ForgotPassword />} />
 				<Route path='/signup' element={authUser ? <Navigate to='/' /> : <SignUp />} />
 			</Routes>
 			<Toaster />
