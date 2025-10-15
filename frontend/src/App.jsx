7ab57d7ce3be282494f9528d484f5d0c5ed23f6f
Divyanshu <divyanshuchoubey8210@gmail.com>
2025-10-16 04:40:46 +0530

    resolvedconflict

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,6 @@
 // frontend/src/App.jsx
 
 import { Navigate, Route, Routes } from "react-router-dom";
-import "./App.css";
 import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
 import SignUp from "./pages/signup/SignUp";
